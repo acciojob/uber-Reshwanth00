44df38514e4230f285d9a7da757b258fcdbccfce
Reshwanth00 <116064259+Reshwanth00@users.noreply.github.com>
2023-01-26 07:00:30 +0530

    Update DriverServiceImpl.java

--- src/main/java/com/driver/services/impl/DriverServiceImpl.java
+++ src/main/java/com/driver/services/impl/DriverServiceImpl.java
@@ -1,23 +1,17 @@
 package com.driver.services.impl;
-
 import com.driver.model.Cab;
 import com.driver.repository.CabRepository;
 import com.driver.services.DriverService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.driver.model.Driver;
 import com.driver.repository.DriverRepository;
-
 @Service
 public class DriverServiceImpl implements DriverService {
-
 	@Autowired
 	DriverRepository driverRepository3;
-
 	@Autowired
 	CabRepository cabRepository3;
-
 	@Override
 	public void register(String mobile, String password){
 		Driver driver = new Driver();
@@ -27,9 +21,7 @@ public class DriverServiceImpl implements DriverService {
 		driver.setCab(cab);
 		cabRepository3.save(cab);
 		driverRepository3.save(driver);
-
 	}
-
 	@Override
 	public void removeDriver(int driverId){
 		driverRepository3.delete(driverRepository3.findById(driverId).get());
@@ -40,6 +32,5 @@ public class DriverServiceImpl implements DriverService {
 		driver.getCab().setAvailable(false);
 		removeDriver(driverId);
 		driverRepository3.save(driver);
-
 	}
 }
