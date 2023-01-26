ed234d4d16070054c501d2a3fbb550ff845b4764
Reshwanth <rachutireshwanth@gmail.com>
2023-01-26 09:20:06 +0530

    admin

--- src/main/java/com/driver/services/impl/CustomerServiceImpl.java
+++ src/main/java/com/driver/services/impl/CustomerServiceImpl.java
@@ -1,16 +1,13 @@
 package com.driver.services.impl;
 
-import com.driver.model.TripBooking;
+import com.driver.model.*;
 import com.driver.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.driver.model.Customer;
-import com.driver.model.Driver;
 import com.driver.repository.CustomerRepository;
 import com.driver.repository.DriverRepository;
 import com.driver.repository.TripBookingRepository;
-import com.driver.model.TripStatus;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,7 +35,33 @@ public class CustomerServiceImpl implements CustomerService {
 	}
 	@Override
 	public TripBooking bookTrip(int customerId, String fromLocation, String toLocation, int distanceInKm) throws Exception{
-
+		//Book the driver with lowest driverId who is free (cab available variable is Boolean.TRUE). If no driver is available, throw "No cab available!" exception
+		//Avoid using SQL query
+		boolean booked = false;
+		List<Driver> driverList = driverRepository2.findAll();
+		TripBooking tripBooking = new TripBooking();
+		for(Driver driver: driverList){
+			if(driver.getCab().getAvailable()){
+				tripBooking.setStatus(TripStatus.CONFIRMED);
+				tripBooking.setFromLocation(fromLocation);
+				tripBooking.setToLocation(toLocation);
+				tripBooking.setDistanceInKm(distanceInKm);
+				tripBooking.setBill(driver.getCab().getPerKmRate()*distanceInKm);
+				tripBookingRepository2.save(tripBooking);
+				Customer customer = customerRepository2.findById(customerId).get();
+				List<TripBooking> customerTripBookingList = customer.getTripBookingList();
+				customerTripBookingList.add(tripBooking);
+				customer.setTripBookingList(customerTripBookingList);
+				customerRepository2.save(customer);
+				List<TripBooking> driverTripBookingList =driver.getTripBookingList();
+				driverTripBookingList.add(tripBooking);
+				driver.setTripBookingList(driverTripBookingList);
+				driverRepository2.save(driver);
+				booked = true;
+			}
+		}
+		if(!booked)throw new Exception("No cab available!");
+		return tripBooking;
 	}
 
 	@Override
